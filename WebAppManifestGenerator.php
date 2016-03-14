3a50b6edb3cc82eedf441c499e2df132cdbbe317
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-03-14 18:34:24 +0000

    Automatically regenerate manifest when adding a field. Fixes #6

--- WebAppManifestGenerator.php
+++ WebAppManifestGenerator.php
@@ -5,11 +5,13 @@ require_once(plugin_dir_path(__FILE__) . '../../marco-c/wp_serve_file/class-wp-s
 if (!class_exists('WebAppManifestGenerator')) {
   class WebAppManifestGenerator {
     private static $instance;
-    private $fields = array(
-      "start_url" => "/",
-    );
+    private $fields;
 
     public function __construct() {
+      $this->fields = get_option('webappmanifest_content', array(
+        "start_url" => "/",
+      ));
+
       add_action('wp_head', array($this, 'add_manifest'));
 
       WP_Serve_File::getInstance()->add_file('manifest.json', array($this, 'manifestJSONGenerator'));
@@ -27,12 +29,10 @@ if (!class_exists('WebAppManifestGenerator')) {
       echo '<link rel="manifest" href="' . WP_Serve_File::get_relative_to_host_root_url('manifest.json') . '">';
     }
 
-    public function regenerate_manifest() {
-      WP_Serve_File::getInstance()->invalidate_files(array('manifest.json'));
-    }
-
     public function set_field($key, $value) {
       $this->fields[$key] = $value;
+      update_option('webappmanifest_content', $this->fields);
+      WP_Serve_File::getInstance()->invalidate_files(array('manifest.json'));
     }
 
     public function manifestJSONGenerator() {
